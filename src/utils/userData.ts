80494b30d8a15d8d69e6267f223a5c69f3ba3622
Hugo M M Brito <97696808+hugommbrito@users.noreply.github.com>
2025-02-11 16:18:03 -0300

    Update userData.ts

--- src/utils/userData.ts
+++ src/utils/userData.ts
@@ -1,7 +1,7 @@
 export const userData = {
   nameUser: "Hugo Brito",
   githubUser: "hugommbrito",
-  linkedinUser: "https://www.linkedin.com/in/hugommbrito/",
+  linkedinUser: "hugommbrito",
   whatsappNumber: "+5583981893971",
   emailUser: "eu@hugobrito.dev.br",
   instagramUser: "hugommbrito",
